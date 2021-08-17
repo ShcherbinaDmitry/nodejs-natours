95b58a8a6a4313fe1f09263ac07d4ed862f9c9b5
Dmitrii Shcherbina <shcherbinadmitry@gmail.com>
2021-08-17 19:34:16 +0300

    adds document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -36,11 +38,11 @@ const tourSchema = new mongoose.Schema(
     summary: {
       type: String,
       trim: true,
+      required: [true, 'A tour must have a description'],
     },
     description: {
       type: String,
       trim: true,
-      required: [true, 'A tour must have a description'],
     },
     imageCover: {
       type: String,
@@ -63,6 +65,16 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
