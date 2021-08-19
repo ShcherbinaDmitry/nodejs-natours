5762b43596390da6d03f68c5a50d76d8096504fa
Dmitrii Shcherbina <shcherbinadmitry@gmail.com>
2021-08-19 15:23:08 +0300

    add error handling

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
-router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router.route('/monthly-plan/:year ').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
